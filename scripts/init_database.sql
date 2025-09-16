6665b09ce3e03439563b08bbdf5bd9a9815a3d90
AnilKumarPaturu <103646033+AnilKumarPaturu@users.noreply.github.com>
2025-09-16 17:54:48 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,15 @@
+--create database 'DataWarehouse'
+
+USE master;
+
+--done
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
